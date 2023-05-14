c27dd2858c2d655cbbc680ab538aa97640893927
wasiushisir <wasiuahmed410@gmail.com>
2023-05-14 21:59:38 +0600

    footer

--- src/Pages/Shop/ShopHome/Home/Products.js
+++ src/Pages/Shop/ShopHome/Home/Products.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Products = () => {
     const state = useSelector((state) => state?.productsReducer?.products?.products)
     return (
-        <div className='md:px-[220px] my-[40px]'>
+        <div className='px-[10px] md:px-[220px] my-[40px]'>
             <h1 className='text-center text-white text-[25px] my-[30px]'>Products of the Shop</h1>
             {state?.length === 0 && <><h1 className='text-center text-white text-[25px] '>There is no Products!!!</h1></>}
             <div className='grid grid-cols-2 md:grid-cols-5 gap-y-2 md:gap-5 ' >
@@ -13,9 +13,9 @@ const Products = () => {
                     state?.map((s) => <>
                         <div class="flex justify-center">
                             {<Link to={`/details/${s._id}`}>
-                                <div class="rounded-lg shadow-lg bg-[#F4F8F9] md:w-[196px] w-[170px]">
+                                <div class="rounded-lg shadow-lg bg-[#F4F8F9] md:w-[196px] w-[160px]">
                                     <a href="#!">
-                                        <img class="rounded-t-lg h-[180px] object-cover w-[170px] md:h-[320px] md:w-[370px]  hover:opacity-70" src={`https://brandatoz.com${s?.image.split(",")[0]}`} alt="" />
+                                        <img class="rounded-t-lg h-[180px] object-cover w-[160px] md:h-[320px] md:w-[370px]  hover:opacity-70" src={`https://brandatoz.com${s?.image.split(",")[0]}`} alt="" />
                                     </a>
                                   
 
