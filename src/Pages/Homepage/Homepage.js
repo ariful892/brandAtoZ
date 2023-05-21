8a5c47d7451ce7b6c4da893e7ede39e5a389225a
wasiushisir <wasiuahmed410@gmail.com>
2023-05-21 14:49:41 +0600

    pic added

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -6,17 +6,23 @@ import './Homepage.css'
 import ProductsForYou from './ProductsForYou/ProductsForYou';
 import ShoppingMalls from './ShoppingMalls/ShoppingMalls';
 import TopSellers from './TopSellers/TopSellers';
-import display from '../../assets/display-picture.png';
-import display2 from '../../assets/display-picture2.png';
-import display3 from '../../assets/display-picture3.png';
-import display4 from '../../assets/display-picture4.png';
+import banner1 from '../../../src/assets/banner1.png'
+import banner2 from '../../../src/assets/banner2.png'
+import banner3 from '../../../src/assets/banner3.png'
+import banner4 from '../../../src/assets/banner4.png'
 import display5 from '../../assets/display-picture5.png';
-import logo1 from '../../assets/logo1.png';
-import logo2 from '../../assets/logo2.png';
-import logo3 from '../../assets/logo3.jpg';
-import logo4 from '../../assets/logo4.png';
-import logo5 from '../../assets/logo5.png';
-import logo6 from '../../assets/logo6.png';
+import feedback2 from '../../assets/2.jpeg'
+import seller2 from '../../assets/1.jpeg'
+import picabo1 from '../../assets/Piacabo_icons/Affiliation.png'
+import picabo2 from '../../assets/Piacabo_icons/C2S payment.png'
+import picabo3 from '../../assets/Piacabo_icons/Cash on delivery.png'
+import picabo4 from '../../assets/Piacabo_icons/Free Delivery.png'
+import picabo5 from '../../assets/Piacabo_icons/Full Bangladesh delivery in three days.png'
+import picabo6 from '../../assets/Piacabo_icons/Shopping malls.png'
+import picabo7 from '../../assets/Piacabo_icons/pay after checking product.png'
+import picabo8 from '../../assets/Piacabo_icons/product Inspection.png'
+import picabo9 from '../../assets/Piacabo_icons/quality guaranteed.png'
+
 import feedback from '../../assets/feedback.png';
 import seller from '../../assets/seller.jpg';
 // import Pagination from './Pagination/Pagination';
@@ -62,7 +68,7 @@ const Homepage = ({ nav, setNav }) => {
                         >
                             <SwiperSlide>
                                 <div className='banner-text'>
-                                    <img className='display-picture' src={display} alt="" />
+                                    <img className='display-picture' src={banner1} alt="" />
 
                                     <div className='ml-5'>
                                         <h2 className='text-2xl lg:text-3xl font-bold mt-4'>Clearance up to 50% off</h2>
@@ -73,7 +79,7 @@ const Homepage = ({ nav, setNav }) => {
                             </SwiperSlide>
                             <SwiperSlide>
                                 <div className='banner-text rounded-lg'>
-                                    <img className='display-picture' src={display2} alt="" />
+                                    <img className='display-picture' src={banner2} alt="" />
 
                                     <div className='ml-5'>
                                         <h2 className='text-2xl lg:text-3xl font-bold mt-4'>Clearance up to 50% off</h2>
@@ -84,7 +90,7 @@ const Homepage = ({ nav, setNav }) => {
                             </SwiperSlide>
                             <SwiperSlide>
                                 <div className='banner-text rounded-lg'>
-                                    <img className='display-picture' src={display3} alt="" />
+                                    <img className='display-picture' src={banner3} alt="" />
 
                                     <div className='ml-5'>
                                         <h2 className='text-2xl lg:text-3xl font-bold mt-4'>Clearance up to 50% off</h2>
@@ -95,7 +101,7 @@ const Homepage = ({ nav, setNav }) => {
                             </SwiperSlide>
                             <SwiperSlide>
                                 <div className='banner-text rounded-lg'>
-                                    <img className='display-picture' src={display4} alt="" />
+                                    <img className='display-picture' src={banner4} alt="" />
 
                                     <div className='ml-5'>
                                         <h2 className='text-2xl lg:text-3xl font-bold mt-4'>Clearance up to 50% off</h2>
@@ -130,38 +136,56 @@ const Homepage = ({ nav, setNav }) => {
                     >
                         <SwiperSlide>
                             <div className='ml-0 lg:ml-12 md:ml-8'>
-                                <img className='w-24 mb-3' src={logo1} alt="" />
-                                <p className='text-xs lg:text-sm'>EASY RETURNS</p>
+                                <img className='w-24 mb-3' src={picabo1} alt="" />
+                                <p className='text-xs lg:text-sm'>Affiliation</p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className='ml-0 lg:ml-12 md:ml-8'>
-                                <img className='w-24 mb-3' src={logo2} alt="" />
-                                <p className='text-xs lg:text-sm'>EASY RETURNS</p>
+                                <img className='w-24 mb-3' src={picabo2} alt="" />
+                                <p className='text-xs lg:text-sm'>C2S Payment</p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className='ml-0 lg:ml-12 md:ml-8'>
-                                <img className='w-24 mb-3' src={logo3} alt="" />
-                                <p className='text-xs lg:text-sm'>EASY RETURNS</p>
+                                <img className='w-24 mb-3' src={picabo3} alt="" />
+                                <p className='text-xs lg:text-sm'>Cash on delivery</p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className='ml-0 lg:ml-12 md:ml-8'>
-                                <img className='w-24 mb-3' src={logo4} alt="" />
-                                <p className='text-xs lg:text-sm'>EASY RETURNS</p>
+                                <img className='w-24 mb-3' src={picabo4} alt="" />
+                                <p className='text-xs lg:text-sm'>Free delivery</p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className='ml-0 lg:ml-12 md:ml-8'>
-                                <img className='w-24 mb-3' src={logo5} alt="" />
-                                <p className='text-xs lg:text-sm'>EASY RETURNS</p>
+                                <img className='w-24 mb-3' src={picabo5} alt="" />
+                                <p className='text-xs lg:text-sm'>Full Bangladesh delivery in three days</p>
                             </div>
                         </SwiperSlide>
                         <SwiperSlide>
                             <div className='ml-0 lg:ml-12 md:ml-8'>
-                                <img className='w-24 mb-3' src={logo6} alt="" />
-                                <p className='text-xs lg:text-sm'>EASY RETURNS</p>
+                                <img className='w-24 mb-3' src={picabo6} alt="" />
+                                <p className='text-xs lg:text-sm'>Shopping malls</p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='ml-0 lg:ml-12 md:ml-8'>
+                                <img className='w-24 mb-3' src={picabo7} alt="" />
+                                <p className='text-xs lg:text-sm'>Pay after checking product</p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='ml-0 lg:ml-12 md:ml-8'>
+                                <img className='w-24 mb-3' src={picabo8} alt="" />
+                                <p className='text-xs lg:text-sm'>Products inspection</p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className='ml-0 lg:ml-12 md:ml-8'>
+                                <img className='w-24 mb-3' src={picabo9} alt="" />
+                                <p className='text-xs lg:text-sm'>Quality guarenteed</p>
                             </div>
                         </SwiperSlide>
                     </Swiper>
@@ -178,7 +202,7 @@ const Homepage = ({ nav, setNav }) => {
                 <div className='my-24 mx-3 lg:mx-5 lg:flex md:flex items-center'>
                     <div className='w-full text-center mr-8' >
                         <div className="bg-white px-5 py-5 shadow-lg rounded-md">
-                            <img className='w-36 mx-auto' src={feedback} alt="" />
+                            <img className='w-36 mx-auto' src={feedback2} alt="" />
                             <h2 className='text-xl font-bold mt-2'>Give us your feedback!</h2>
                             <p className='mt-1'>Please help us to improve. Your feedback is important and helps us to grow more.</p>
                             <button className='btn-warning px-3 py-1 mt-3 rounded-lg font-semibold text-md'>Feedback</button>
@@ -187,7 +211,7 @@ const Homepage = ({ nav, setNav }) => {
 
                     <div className='w-full text-center mt-8 md:mt-0 lg:mt-0' >
                         <div className="bg-white px-5 py-5 shadow-lg rounded-md">
-                            <img className='w-36 mx-auto' src={seller} alt="" />
+                            <img className='w-36 mx-auto' src={seller2} alt="" />
                             <h2 className='text-xl font-bold mt-2'>Become a seller!</h2>
                             <p className='mt-1'>Sell Online - Grow your business with BrandAtoZ, an online selling platform. Now sell products online & become a top e-commerce seller.</p>
                             <button className='btn-warning px-3 py-1 mt-3 rounded-lg font-semibold text-md'>Become Seller</button>
